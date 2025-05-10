73a0c1afeed30a9d7a99181292ea214e913ecdd0
Easha Akram <eashaakram63@gmail.com>
2025-05-10 13:20:19 +0500

    Diamond Shape Code
    In this code we will print Diamond shape by using asterisk ( * ) symbol.

---
+++ Diamond Shape.cpp
@@ -0,0 +1,16 @@
+  #include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"Printing Diamond!!!"<<endl;	
+	for(int i=0; i<5; i++)
+	{
+		for(int j=0; j<5; j++)
+		{
+			if((i==0&&j==2) || (i==1&&(j==1||j==3)) || (i==2&&(j==0||j==4)) || (i==3&&(j==1||j==3)) || (i==4&&j==2))
+			cout<<"*";
+			cout<<"\t";
+		}
+		cout<<"\n";
+	}
+}
