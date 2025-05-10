ee2d89710e7fc14a2e742b7ab20cc85c8d1e608c
Easha Akram <eashaakram63@gmail.com>
2025-05-10 13:14:19 +0500

    Triangle Shape Code
    In this code we will print Triangle by using asterisk ( * ) symbol.

---
+++ Triangle Shape.cpp
@@ -0,0 +1,14 @@
+  #include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"Printing Triangle!!!"<<endl;
+             for (int i = 0; i < 5; i++)  
+        {
+                for (int j = 0; j <= i ; j++)
+		{
+                cout << "*"; 
+                }
+              cout << "\n"; 
+        }
+}
