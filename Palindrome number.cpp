93b98e67fc42add9ea816c31374bbf7905f08850
Easha Akram <eashaakram63@gmail.com>
2025-05-11 13:13:25 +0500

    Palindrome Number Checker
    This is a simple C++ program that checks whether a number is a palindrome — meaning it reads the same forwards and backwards (e.g., 121, 1331).
    The program takes user input, reverses the number, and compares it with the original. If both are equal, it prints that the number is a palindrome.

---
+++ Palindrome number.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int function();
+int main(){
+	cout<<"Palindrome Numbers!!"<<endl;
+	int number;
+	int reverse = 0;
+	int original;
+	int digit;
+	cout<<"Enter any number: ";
+	cin>>number;
+	original = number;
+	while(number != 0)
+	{
+		digit = number%10;
+		reverse = reverse*10+digit;
+		number = number/10;
+	}
+		if(reverse == original)
+		{
+			cout<<"The number is "<<original<<"\nAfter reversing the number remains same which is "<<reverse<<endl;
+			cout<<"So, Yes this number is Palindrome!"<<endl;
+		}
+		else
+		{
+			cout<<"The number is "<<original<<"\nAfter reversing the number does not remains same which is "<<reverse<<endl;
+			cout<<"So, The number is not Palindrome!"<<endl;
+		}
+	return 0;
+}
