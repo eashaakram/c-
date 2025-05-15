c14d6ec1f69cef73721183bf3ac75c02fb224d62
Easha Akram <eashaakram63@gmail.com>
2025-05-15 09:03:50 +0500

    Square Root Built-in Function

---
+++ Square root Built-in Function.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+	double x;
+	cout<<"Enter a number of which square root u want: ";
+	cin>>x;
+	cout<<"Square root of "<<x<<" is "<<sqrt(x)<<endl;
+	return 0;
+}
