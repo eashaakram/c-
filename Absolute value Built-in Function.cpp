6044d2d93eccbe193ce213016421ee96362e1991
Easha Akram <eashaakram63@gmail.com>
2025-05-20 00:38:59 +0500

    Absolute Value Built-in Function

---
+++ Absolute value Built-in Function.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+	cout<<"Absolute Value"<<endl;
+	double x=-10;
+	cout<<"Enter any number in negative : ";
+	cin>>x;
+
+	cout<<"abs(-10) = "<<fabs(x)<<endl; //Returns 10
+	return 0;
+}
+
