255620e573264ea938a1b80935322bf45919f452
Easha Akram <eashaakram63@gmail.com>
2025-05-20 20:42:39 +0500

    Ceil Built-in Function

---
+++ Ceil Built-in Function.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+	cout<<"Ceil(x)_Round up"<<endl;
+	double num = 4.2;
+	cout<<" Ceil(4.2) = "<<ceil(num)<<endl; //Rounds up to 5
+	return 0;
+}
