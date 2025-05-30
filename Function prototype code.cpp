1abc9de4feb4a73d00f36e4294e0bee1cc5fadac
Easha Akram <eashaakram63@gmail.com>
2025-05-30 16:16:50 +0500

    How to use Function prototype?

---
+++ Function prototype code.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int deeper();
+int deep();
+int main(){
+	cout<<"I'm in main function"<<endl;
+	deep();
+	cout<<"Now I'm back in main function"<<endl;
+}
+int deeper(){
+	cout<<"I'm in deeper function"<<endl;
+}
+int deep(){
+	cout<<"I'm in deep function"<<endl;
+	deeper();
+	cout<<"Again I'm in deep function"<<endl;
+}
