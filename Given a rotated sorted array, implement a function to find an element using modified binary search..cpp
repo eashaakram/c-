3fbdb5126311d993c8f8a319dc7338addfadb1da
Easha Akram <eashaakram63@gmail.com>
2025-06-01 22:16:33 +0500

    Given a rotated sorted array, implement a function to find an element using modified binary search..cpp

--- Given a rotated sorted array, implement a function to find an element using modified binary search..cpp
+++ Given a rotated sorted array, implement a function to find an element using modified binary search..cpp
@@ -6,7 +6,7 @@ int main(){
 	int array[size]={6, 7, 8, 9, 1, 2, 3, 4, 5};
 	
 	for(int i=0; i<size-1; i++){
-		for(int j=0; j<size-i; j++){
+		for(int j=0; j<size-1-i; j++){
 			if(array[j]>array[j+1]){
 			int temp=array[j];
 			array[j]=array[j+1];
