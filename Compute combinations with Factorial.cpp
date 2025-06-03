32589fad6434e7cedd395a486adc7b99ad720efb
Easha Akram <eashaakram63@gmail.com>
2025-06-03 22:28:19 +0500

    Compute combinations with Factorial

---
+++ Compute combinations with Factorial.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+long long factorial(int);
+int main(){
+	long long fn, fr, fnr, C;
+	int n=10, r=3;
+	fn=factorial(n);
+	fr=factorial(r);
+	fnr=factorial(n-r);
+	C=fn/(fr*fnr);
+	cout<<"C("<<n<<":"<<r<<") = "<<C<<endl;	
+}
+long long factorial(int num){
+	int fact=1, i=1;
+	while(i<=num){
+		fact=fact*i;
+		i++;
+	}
+	return fact;
+}
