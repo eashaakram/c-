4fa502c8a536ba4459d9323d716cfb4181aab593
Easha Akram <eashaakram63@gmail.com>
2025-10-29 01:27:18 +0500

    inheritance2

--- OOP/inheritance2.cpp
+++ OOP/inheritance2.cpp
@@ -5,24 +5,27 @@ class Person{
 	public:
 		string name;
 		int age;
-		Person(){
-			cout<<"Parent Constructor..\n";
-		}
+	
 		Person(string n, int ag){
+			cout<<"Parent Constructor..\n";
 			name=n;
 			age=ag;
+		}
+		~Person(){
+			cout<<"Parent Destructor..\n";
 		}
 	};
 	//derived class(CHILD)
 	class Student : public Person{
 		public:
 			int rollno;
-			Student(){
-				cout<<"Child Constructor..\n";
-			
-			}
 			Student(string n,int ag,int roll):Person(n,ag){
+					cout<<"Child Constructor..\n";
 				rollno=roll;	
+			}
+			~Student(){
+				cout<<"Child Destructor..\n";
+			
 			}
 			void getInfo(){
 				cout<<"Name: "<<name<<"\nAge: "<<age<<"\nRoll no. : "<<rollno<<endl;
