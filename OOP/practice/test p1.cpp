81997b36512a9a6c538124f746038a1541693fe2
Easha Akram <eashaakram63@gmail.com>
2025-10-30 13:51:20 +0500

    Refactor Teacher class and main function

--- OOP/practice/test p1.cpp
+++ OOP/practice/test p1.cpp
@@ -26,3 +26,34 @@ t2.show();
 Teacher t3(3,"Sana","SE","Calc",70000);
 t3.show();
 }
+//Another way
+// #include<iostream>
+// using namespace std;
+// class Teacher{
+// 	private:
+// 		double salary;
+// 	public:
+// 	int num;
+// 	string name, dept, course;
+// 	void setvalue(double sal){
+// 		salary = sal;
+// 	}
+// 	double getvalue(){
+// 		return salary;
+// 	}
+// 	void show(int num, string name, string dept, string course, double sal){
+// 		cout<<"\tDATA of Teacher#";
+// 			cout<<num<<"\nName: "<<name<<"\tDepartment: "<<dept<<"\tCourse: "<<course<<"\tSalary: "<<salary<<endl;
+// 	}
+// };
+// int main(){
+// Teacher t1;
+// t1.setvalue(50000);
+// t1.show(1,"EESHA","SE","OOP",t1.getvalue());
+// Teacher t2;
+// t2.setvalue(80000);
+// t2.show(2,"Nimra","CS","OOP",t2.getvalue());
+// Teacher t3;
+// t3.setvalue(70000);
+// t3.show(3,"Sana","SE","Calc",t3.getvalue());
+// }
